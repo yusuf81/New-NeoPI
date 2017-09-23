7c15c093c708cdb329177ac67782100efc8a8990
Mohammad Yusuf Syafroni Karim <syafroni@aol.com>
2017-09-23 15:25:50 +0700

    error saalh keitk
    error saalh keitk

--- neopi.py
+++ neopi.py
@@ -542,12 +542,12 @@ class SearchFile:
                     if valid_regex.search(file) and os.path.getsize(filename) > SMALLEST:
                         try:
                             data = open(root + "/" + file, 'rb').read()
-                        except (OSError, IOError):
+                        except: (OSError, IOError):
                             data = False
                             raise
                         finally:
                             yield data, filename
-                    except (OSError, IOError):
+                    except: (OSError, IOError):
                         print "Could not read file :: %s/%s" % (root, file)
 
 if __name__ == "__main__":
